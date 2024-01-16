fe8dac02ee745522cc71b239c0a4d24adab43ca5
rinaharry <ramandimbisoarinah@gmail.com>
2024-01-16 16:26:58 +0300

    .gitignore is now working

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,40 @@
-use actix_web::{web, App, HttpServer, dev::Url};
+
+use actix_web::{get, http, web, App, HttpRequest, HttpResponse, HttpServer};
 use log::info;
 use actix_cors::Cors;
 use dotenv::dotenv;
 use std::env;
+use postgres::{ Client, NoTls };
+use postgres::Error as PostgresError;
 
 
 #[derive(serde::Deserialize)]
 struct Info {
   name: String,
 }
+
+//DATABASE URL
+const DB_URL: &str = env!("DATABASE_URL");
+
+
+
+//db setup
+fn set_database() -> Result<(), PostgresError> {
+    let mut client = Client::connect(DB_URL, NoTls)?;
+    client.batch_execute(
+        "
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            email VARCHAR NOT NULL
+        )
+    "
+    )?;
+    Ok(())
+}
+
+
+
 
 // Basic GET / endpoint
 async fn index() -> &'static str {
@@ -31,7 +57,10 @@ async fn main() -> std::io::Result<()> {
     let port_url = env::var("PORT").expect("PORT_URL not found in .env");
     let url = format!("127.0.0.1:{PORT}", PORT = port_url);
     let server = HttpServer::new(|| {
+        let cors = Cors::default()
+                             .allow_any_origin().send_wildcard();
         App::new()
+            .wrap(cors)
             .route("/", web::get().to(index))
             .route("/info", web::post().to(create_info))
             
