024565125036c289f357131fafce1589ba5c223c
rinaharry <ramandimbisoarinah@gmail.com>
2024-01-16 16:27:47 +0300

    Init db

--- server/src/main.rs
+++ server/src/main.rs
@@ -13,25 +13,25 @@ struct Info {
   name: String,
 }
 
-//DATABASE URL
-const DB_URL: &str = env!("DATABASE_URL");
+// //DATABASE URL
+// const DB_URL: &str = env!("DATABASE_URL");
 
 
 
-//db setup
-fn set_database() -> Result<(), PostgresError> {
-    let mut client = Client::connect(DB_URL, NoTls)?;
-    client.batch_execute(
-        "
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL,
-            email VARCHAR NOT NULL
-        )
-    "
-    )?;
-    Ok(())
-}
+// //db setup
+// fn set_database() -> Result<(), PostgresError> {
+//     let mut client = Client::connect(DB_URL, NoTls)?;
+//     client.batch_execute(
+//         "
+//         CREATE TABLE IF NOT EXISTS users (
+//             id SERIAL PRIMARY KEY,
+//             name VARCHAR NOT NULL,
+//             email VARCHAR NOT NULL
+//         )
+//     "
+//     )?;
+//     Ok(())
+// }
 
 
 
