50ad84ec8cb255e28e51353bcede2bae6236c80d
rinaharry <ramandimbisoarinah@gmail.com>
2024-01-22 16:55:33 +0300

    Init api

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,75 +1,65 @@
 
-use actix_web::{get, http, web, App, HttpRequest, HttpResponse, HttpServer};
-use log::info;
-use actix_cors::Cors;
-use dotenv::dotenv;
-use std::env;
-use postgres::{ Client, NoTls };
-use postgres::Error as PostgresError;
 
+extern crate diesel;
+
+use actix_cors::Cors;
+use actix_web::{middleware, web, App ,post, HttpResponse, HttpServer};
+use dotenv::dotenv;
+
+use std::env;
+use log::{debug, error, log_enabled, info, Level};
+use env_logger;
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+// We define a custom type for connection pool to use later.
+pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[derive(serde::Deserialize)]
-struct Info {
+struct InfoPingPong {
   name: String,
 }
 
-// //DATABASE URL
-// const DB_URL: &str = env!("DATABASE_URL");
-
-
-
-// //db setup
-// fn set_database() -> Result<(), PostgresError> {
-//     let mut client = Client::connect(DB_URL, NoTls)?;
-//     client.batch_execute(
-//         "
-//         CREATE TABLE IF NOT EXISTS users (
-//             id SERIAL PRIMARY KEY,
-//             name VARCHAR NOT NULL,
-//             email VARCHAR NOT NULL
-//         )
-//     "
-//     )?;
-//     Ok(())
-// }
-
-
-
-
 // Basic GET / endpoint
 async fn index() -> &'static str {
-    "Hello world!"
+    "Server is ok!"
 }
 
-// POST /info endpoint to create info
-async fn create_info(info: web::Json<Info>) -> String {
-    format!("Welcome {}!", info.name) 
+// POST /info endpoiDbPoolnt to create info
+async fn create_info(res: web::Json<InfoPingPong>) -> String {
+    format!("Welcome {}!", res.name) 
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+
     // Initialize logger
-    env_logger::init();
-    dotenv().ok();
+        env_logger::init();
+        dotenv().ok(); 
 
-    info!("Starting actix-web server...");
+      // set up database connection pool
+      let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not found in .env");
+      let manager = ConnectionManager::<PgConnection>::new(database_url);
+      let pool: DbPool = r2d2::Pool::builder()
+          .build(manager)
+          .expect("Failed to create pool.");
+        debug!("Starting actix-web server...");
+        info!("Starting actix-web server...");
 
-    let port_url = env::var("PORT").expect("PORT_URL not found in .env");
-    let url = format!("0.0.0.0:{PORT}", PORT = port_url);
-    let server = HttpServer::new(|| {
-        let cors = Cors::default()
-                             .allow_any_origin().send_wildcard();
-        App::new()
-            .wrap(cors)
+        let port_url: String = env::var("PORT").expect("PORT_URL not found in .env");
+        let url: String = format!("0.0.0.0:{PORT}", PORT = port_url);
+        
+        let server = HttpServer::new(move || {
+          App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .wrap(middleware::Logger::default())
+            .wrap( Cors::permissive())
             .route("/", web::get().to(index))
-            .route("/info", web::post().to(create_info))
-            
+            .route("/info", web::post().to(create_info))    
     })
     .bind(url)?;
 
     server.run().await?;
-
-    info!("Server has shut down");
-
+    debug!("Server has shut down");
     Ok(())
+
 }
